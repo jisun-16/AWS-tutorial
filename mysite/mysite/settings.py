18e7e0651a171ebbe9160c594aef3c31c2ee6b92
lim ji sun <limjisun97@naver.com>
2020-11-18 22:07:45 +0900

    collectstatic

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
